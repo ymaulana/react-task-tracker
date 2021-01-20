e408a60bff856cfc1ccff90770b504e4059686cf
ymaulana <y.maulana1609@gmail.com>
2021-01-21 04:56:49 +0700

    create tasks file to wrap each of task
    create task file to display task
    
    adding react-icons to display delete button
    
    adding delete button function on App file
    
    adding doubleclick function on task for marking the task is active

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Checkout Mail",
+      day: "Jan 20th at 08:00pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting",
+      day: "Jan 20th at 10:00pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shoping",
+      day: "Jan 20th at 01:00pm",
+      reminder: false,
+    },
+  ]);
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Task To Show"
+      )}
     </div>
   );
 };
